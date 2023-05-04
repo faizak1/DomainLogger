fe4d718d2ffeaa58c5c0084f4fe5a84063f22cf5
Faiza Khan <faizakhan@Faizas-MBP.home>
2023-05-03 23:53:54 -0400

    HW8: Enhanced UI screen and increased code coverage to 98 percent

--- tests/popup.integration.test.js
+++ tests/popup.integration.test.js
@@ -50,7 +50,7 @@ describe("deleteTabs", () => {
     const headers = table.querySelectorAll("strong");
     expect(headers.length).toBe(3);
     expect(headers[0].textContent).toBe("DOMAINS");
-    expect(headers[1].textContent).toBe("VISITS/TAB COUNT");
+    expect(headers[1].textContent).toBe("TAB COUNT");
     expect(headers[2].textContent).toBe("DELETE TABS");
 
     const rows = table.querySelectorAll("tr");
@@ -61,5 +61,24 @@ describe("deleteTabs", () => {
     expect(rows[2].textContent).toContain("4"); // 4 tabs with www.google.com
     expect(rows[3].textContent).toContain("twitter.com");
     expect(rows[3].textContent).toContain("1"); // 1 tab with twitter.com
+
+    /*chrome.tabs.query.mockImplementation((queryInfo, callback) => {
+      callback(tabs);
+    });*/
+
+    // Click on an existing domain
+    /* const clickEvent = new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+      view: window
+    });
+    const existingDomain = "https://www.google.com";
+    const existingTarget = document.createElement("div");
+    existingTarget.textContent = existingDomain;
+    document.body.appendChild(existingTarget);
+    existingTarget.dispatchEvent(clickEvent);
+  
+    // Check that chrome.tabs.remove is called
+    expect(chrome.tabs.remove).toHaveBeenCalled();*/
   });
 });
